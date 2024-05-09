185095ec237a16a0fbb4d2b0d8afcd98b2ecbe6a
Misstes <you@example.com>
2024-05-09 09:32:42 -0500

    feat: Add Swagger integration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth NestJS Documentation | Van Rossum')
+    .setDescription(
+      'Handling Riwi Coin user authentication\n\n ▪️ Hernán Pereira \n\n ▪️ Samuel Vera',
+    )
+    .setVersion('1.0')
+    .addTag('Auth')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
+
   const port = process.env.port || 3001;
 
   await app.listen(port);
   console.log(
-    `💜 Server on port: ${port} \n💜 http://localhost:${port}/api/v1`,
+    `💜 Server on port: ${port} \n▪️ Server:\n💜 http://localhost:${port}/api/v1 \n\n▪️ Documentation:\n💜 http://localhost:${port}/api-doc`,
   );
 }
 bootstrap();
