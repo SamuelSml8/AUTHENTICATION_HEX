be6d003a91edeb9bd1d3df5a6e169e45bf81c618
Samuel Sml <veramirandasamuel6@gmail.com>
2024-05-09 08:44:18 -0500

    Feature: Start server with dynamic port configuration

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('/api/v1');
+  const port = process.env.port || 3001;
+
+  await app.listen(port);
+  console.log(
+    `💜 Server on port: ${port} \n💜 http://localhost:${port}/api/v1`,
+  );
 }
 bootstrap();
