72490b587f65458578e155970f6674b2954fa956
Samuel Sml <veramirandasamuel6@gmail.com>
2024-05-10 14:26:14 -0500

    feat: Create UserRepository interface with the use of JSONResponse from common module
    The UserRepository interface has been created to define operations for the user repository. This interface uses the JSONResponse interface from the common module to ensure consistent response formatting across the application.

---
+++ src/modules/users/domain/repositories/user-repository.interface.ts
@@ -0,0 +1,14 @@
+import { JSONResponse } from 'src/common/json-response.interface';
+import { CreateUserDto, updateUserDto } from '../../application/dtos';
+import { User } from '../entities/users.entity';
+
+export interface UserRepository {
+  createUser(createUserDto: CreateUserDto): Promise<JSONResponse<User>>;
+  findUserById(id: string): Promise<JSONResponse<User>>;
+  findAllUsers(): Promise<JSONResponse<User[]>>;
+  updateUser(
+    id: string,
+    updateUserDto: updateUserDto,
+  ): Promise<JSONResponse<User>>;
+  deleteUser(id: string): Promise<JSONResponse<void>>;
+}
