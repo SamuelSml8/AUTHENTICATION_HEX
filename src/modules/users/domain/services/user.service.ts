f2606147f3f1313ae3713b394921dc31feb8514d
Samuel Sml <veramirandasamuel6@gmail.com>
2024-05-10 21:58:33 -0500

    feat: Implement UserService to handle user operations
    This commit adds the implementation of the UserService in the domain layer to handle operations related to users, such as creating, searching, updating, and deleting users. The service utilizes dependency injection to access the UserRepository and follows the principles of hexagonal architecture by maintaining a clear separation of layers and responsibilities.

--- src/modules/users/domain/services/user.service.ts
+++ src/modules/users/domain/services/user.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from '../repositories/user-repository.interface';
+import { CreateUserDto, updateUserDto } from '../../application/dtos';
+import { JSONResponse } from 'src/common/json-response.interface';
+import { User } from '../entities/users.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<JSONResponse<User>> {
+    return await this.userRepository.createUser(createUserDto);
+  }
+
+  async findUserById(id: string): Promise<JSONResponse<User>> {
+    return await this.userRepository.findUserById(id);
+  }
+
+  async findAllUsers(): Promise<JSONResponse<User[]>> {
+    return await this.userRepository.findAllUsers();
+  }
+
+  async updateUser(
+    id: string,
+    updateUserDto: updateUserDto,
+  ): Promise<JSONResponse<User>> {
+    return await this.userRepository.updateUser(id, updateUserDto);
+  }
+
+  async deleteUser(id: string): Promise<JSONResponse<void>> {
+    return await this.userRepository.deleteUser(id);
+  }
+}
